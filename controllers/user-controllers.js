b66d52aa790ce45dbd91f1788f1de5daf3707dd2
playcsgo <sakura_0125@hotmail.com>
2024-07-18 00:56:59 +0800

    remove console

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,12 +1,24 @@
 // controllers/user-controller
 class UserController {
   constructor({ userModel }) {
-    console.log('[this from constructor]', this)
     this.userModel = userModel
     this.createUser = this.createUser.bind(this)
   }
 
   async createUser(request, reply) {
+    const { name, email, password, found } =  request.body
+    const createdUser = await this.userModel.create({
+      name,
+      email,
+      password,
+      found: Number(found)
+    })
+
+    return { createdUser }
+  }
+
+  //other method
+  async getUser(request, reply) {
     const { name, email, password, found } =  request.body
     console.log('[this from createUser]', this)
     const createdUser = await this.userModel.create({
@@ -19,7 +31,6 @@ class UserController {
     return { createdUser }
   }
 
-  //other method
   async bet() {
     
   }
